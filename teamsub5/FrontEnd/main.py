c3a0d8d9e4d08f46010b87862fe9119139cffd08
lee-cheolwoo <cjf4674@dgu.ac.kr>
2024-05-17 22:39:36 +0900

    team project homework5

---
+++ teamsub5/FrontEnd/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+@app.route('/')
+def input():
+    return render_template('input_info.html')
+
+@app.route('/result',methods=['POST','GET'])
+def result():
+    if request.method =='POST':
+       result=dict()
+       result['Name']=request.form.get('name')
+       result['Student Number']=request.form.get('StudentNumber')
+       result['University'] = request.form.get('University')
+       result['Gender'] = request.form.get('gender')
+       result['Major'] = request.form.get('major')
+       email_id = request.form.get('email_id')
+       email_domain = request.form.get('email_domain')
+       result['Email'] = f"{email_id}@{email_domain}"
+       result['Languages'] = ', '.join(request.form.getlist('languages'))
+       
+       return render_template('result.html',result=result)
+
+if __name__ =='__main__':
+     app.run(debug=True)
