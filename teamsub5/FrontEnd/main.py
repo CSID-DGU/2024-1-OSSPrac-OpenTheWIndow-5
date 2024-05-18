d93401f732f1eaee842f1367d962264856df6370
HyunJeong Kim <betty00507@dgu.ac.kr>
2024-05-18 22:49:46 +0900

    Modify input_info

--- teamsub5/FrontEnd/main.py
+++ teamsub5/FrontEnd/main.py
@@ -15,9 +15,7 @@ def result():
        result['University'] = request.form.get('University')
        result['Gender'] = request.form.get('gender')
        result['Major'] = request.form.get('major')
-       email_id = request.form.get('email_id')
-       email_domain = request.form.get('email_domain')
-       result['Email'] = f"{email_id}@{email_domain}"
+       result['Email']='@'.join(request.form.getlist('email'))
        result['Languages'] = ', '.join(request.form.getlist('languages'))
        
        return render_template('result.html',result=result)
